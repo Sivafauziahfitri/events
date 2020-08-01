735cadbb53e961692e79997ab99324eb16921a2a
sivafauziahfitri <sivafauziahftr@gmail.com>
2020-08-01 14:17:36 +0700

    first commit

---
+++ database/migrations/2020_08_01_053004_create_kegiatans_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKegiatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kegiatans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kode_kegiatan')->unique();
+            $table->string('nama_kegiatan');
+            $table->string('tanggal');
+            $table->string('keterangan');
+            $table->string('status_kegiatan');
+            $table->string('harga_tiket');
+            $table->string('file');
+            $table->string('kapasitas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kegiatans');
+    }
+}
---
+++ database/migrations/2020_08_01_054535_create_siswas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('nisn');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->string('nomor_telpnumber');
+            $table->string('alamat');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
